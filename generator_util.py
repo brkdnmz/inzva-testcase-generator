ba52304565b3ae537965f07a99aaa08ea2ef9d6a
Berke Dönmez <brkdnmz99@gmail.com>
2023-10-10 00:45:13 +0300

    Add playground notebook, slight fix to is_prime

--- generator_util.py
+++ generator_util.py
@@ -116,6 +116,12 @@ def is_prime(num: int) -> bool:
     Returns:
         bool: `true` if `num` is a prime, otherwise `false`.
     """
+
+    assert num >= 0, "`num` should not be negative"
+
+    if num <= 1:
+        return False
+
     for i in range(2, num + 1):
         if i * i > num:
             break
@@ -153,6 +159,7 @@ def next_prime(n: int) -> int:
     Returns:
         int: Smallest `p` such that `p > n` and `p` is a prime.
     """
+
     n += 1
     while not is_prime(n):
         n += 1
